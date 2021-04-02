61696e54f34708a9d0b4e51725af667b19545789
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-04-02 22:20:43 +0530

    create route for output page

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ def index():
 
 @app.route('/output', methods=['GET', 'POST'])
 def output():
-    return render_template('output.html')
+    text = request.form.get("enter")
+    data = text
+    return render_template('output.html', data=text)
 
 
 if __name__ == "__main__":
