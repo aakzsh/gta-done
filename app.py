4e103d1a77509cdd8d2fab1b8dad47beba252fb4
Tejas Kannampuzha <tejaskannampuzha@gmail.com>
2021-10-21 11:28:10 +0530

    Fixed bug at line number 42. Imporved code at line number 20 and 22 using islower() and isupper().

--- app.py
+++ app.py
@@ -12,66 +12,15 @@ def index():
 @app.route('/output', methods=['GET', 'POST'])
 def output():
     text = request.form.get("enter")
-    length = len(text)
-    news = []
     i = 0
-    line = 0
     flags = []
     for x in text:
         i += 1
 
-        if x == 'A':
-            flags.append("static/Aa.png")
-        elif x == 'B':
-            flags.append("static/Bb.png")
-        elif x == 'C':
-            flags.append("static/Cc.png")
-        elif x == 'D':
-            flags.append("static/Dd.png")
-        elif x == 'E':
-            flags.append("static/Ee.png")
-        elif x == 'F':
-            flags.append("static/Ff.png")
-        elif x == 'G':
-            flags.append("static/Gg.png")
-        elif x == 'H':
-            flags.append("static/Hh.png")
-        elif x == 'I':
-            flags.append("static/Ii.png")
-        elif x == 'J':
-            flags.append("static/Jj.png")
-        elif x == 'K':
-            flags.append("static/Kk.png")
-        elif x == 'L':
-            flags.append("static/Ll.png")
-        elif x == 'M':
-            flags.append("static/Mm.png")
-        elif x == 'N':
-            flags.append("static/Nn.png")
-        elif x == 'O':
-            flags.append("static/Oo.png")
-        elif x == 'P':
-            flags.append("static/Pp.png")
-        elif x == 'Q':
-            flags.append("static/Qq.png")
-        elif x == 'R':
-            flags.append("static/Rr.png")
-        elif x == 'S':
-            flags.append("static/Ss.png")
-        elif x == 'T':
-            flags.append("static/Tt.png")
-        elif x == 'U':
-            flags.append("static/Uu.png")
-        elif x == 'V':
-            flags.append("static/Vv.png")
-        elif x == 'W':
-            flags.append("static/Ww.png")
-        elif x == 'X':
-            flags.append("static/Xx.png")
-        elif x == 'Y':
-            flags.append("static/Yy.png")
-        elif x == 'Z':
-            flags.append("static/Zz.png")
+        if x.islower():
+            flags.append("static/" + x + ".png")
+        elif x.isupper():
+            flags.append("static/" + x + x.lower() + ".png")
         elif x == '?':
             flags.append("static/qmark.png")
         elif x == '/':
@@ -84,13 +33,16 @@ def output():
             flags.append("static/_(1).png")
         elif x == ',':
             flags.append("static/,.png")
-        elif x == ' ':
-            line = line + 1
+        elif x == ' ' or '':
             flags.append("static/space.png")
         elif x == ';':
             flags.append("static/semicolon.png")
+        elif x=='\n':
+            flags.append('end')
+        elif x=='\r':#This line is very essential, its due to a new line character.
+            flags.append("static/space.png")
         else:
-            flags.append("static/" + x + ".png")
+            flags.append(f"static/{x}.png")
 
     return render_template("output.html", flags=flags, text=text)
 
